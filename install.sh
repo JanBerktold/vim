ce02f195bdea5c232d77bb354bb4f9a3f95bd186
Jan Berktold <jberktold@roblox.com>
2018-04-02 00:03:45 -0700

    Add homebrew installation for os x

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ] ; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+# install homebrew if we're on os x
+if [[ 'uname -s' == 'Darwin' ]]; then
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
