1c30f1d9cbcaac74df468ce7f02615dbff429b2a
Jan Berktold <jberktold@roblox.com>
2018-04-02 00:29:30 -0700

    Add YouCompleteMe on mac

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ] ; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+vim +PluginInstall +qall
+
 # install homebrew if we're on os x
 if [[ 'uname -s' == 'Darwin' ]]; then
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -14,6 +16,17 @@ fi
 
 if [[ -x "$(command -v brew)" ]]; then
 	brew install tmux
+	brew install cmake
 else
 	echo "Did not find any comnpatible package manager"
+fi
+
+# install YouCompleteMe
+if [[ -x "$(command -v brew)" ]]; then
+	cd ~/.vim/bundle
+	rm -rf YouCompleteMe/ycmbuild
+	mkdir YouCompleteMe/ycmbuild
+	cd YouCompleteMe/ycmbuild
+	cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+	make ycm_core
 fi
