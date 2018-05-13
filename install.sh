498aa2b6849ca4829a58a570f1e80def1ef7b8f6
Jan Berktold <jan@berktold.co>
2018-05-13 09:48:15 -0700

    Add zsh

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ set -e
 ln -sf $(PWD)/vimrc ~/.vimrc # setup symlink for .vimrc
 
 if [ ! -d ~/.vim/bundle/Vundle.vim ] ; then
+	cwd=$(pwd)
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	cd ~/.vim/bundle/Vundle.vim
+	git submodule update --init --recursive
+	cd $cwd
 fi
 
 
@@ -16,6 +20,7 @@ fi
 if [[ -x "$(command -v brew)" ]]; then
 	brew install mas 	# interface to app store
 	mas install 497799835 	# XCode
+				# TODO: XCode has to be launched before installing macvim to install additional components.
 	mas install 1295203466	# Microsoft Remote Desktop 10.1.8
 	brew install tmux
 	brew install cmake
@@ -30,11 +35,18 @@ if [[ -x "$(command -v brew)" ]]; then
 	brew cask install sublime-text
 	brew cask install intellij-idea
 	brew cask install iterm2
+
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	ln -sf $(PWD)/.zshrc ~/.zshrc # setup symlink for .zshrc
+
 else
 	echo "Did not find any comnpatible package manager"
 fi
 
 vim +PluginInstall +qall
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+
 ~/.vim/bundle/YouCompleteMe/install.py --java-completer --go-completer
 
 # setup Go for YouCompleteMe
--- vimrc
+++ vimrc
@@ -5,14 +5,12 @@ set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
 Plugin 'VundleVim/Vundle.vim'
 Plugin 'Valloric/YouCompleteMe'
-Plugin 'altercation/vim-colors-solarized'
 Plugin 'itchyny/lightline.vim'
 Plugin 'airblade/vim-gitgutter'
 call vundle#end()
 
 syntax enable
 set background=dark
-colorscheme solarized
 
 filetype plugin indent on
 
